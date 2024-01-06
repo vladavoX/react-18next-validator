9e189c961a59401488c091d8b14a09555a949cbd
Vladimir Aleksic <vlada.pzs@hotmail.com>
2024-01-06 21:37:08 +0100

    re-generate docs, change folder to /docs

--- ri18next.config.js
+++ ri18next.config.js
@@ -1,9 +1,8 @@
 /**
- * @typedef {{errorLevel?: 'warn'|'error'|'off', regex?: Array<string>, src?: string, directory?: string}} Config
+ * @typedef {{errorLevel: 'warn'|'error'|'off', regex: Array<string>, src: string, directory: string}} Config
  * @description The default config for the validator
- * @property {string} src The source folder
- * @property {'warn'|'error'|'off'} errorLevel The error level
- * @property {Array<string>} regex The regex to find the translation keys in the source files
+ * @property {'warn'|'error'|'off'} [errorLevel] The error level
+ * @property {Array<string>} [regex] The regex to find the translation keys in the source files
  * @property {string} src The source folder
  * @property {string} directory The directory of the translation files
  */
