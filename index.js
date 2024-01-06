b62eb253a29c6bd71ce02eb70c509bcb7152c44f
Vladimir Aleksic <vlada.pzs@hotmail.com>
2024-01-06 18:43:31 +0100

    Add ability to create custom config

--- index.js
+++ index.js
@@ -1,5 +1,28 @@
 #!/usr/bin/env node
-import { config } from './ri18next.config.js'
+import fs from 'fs'
+import path from 'path'
+import * as Types from './ri18next.config.js'
+
+const configFile = 'ri18next-v.config.json'
+const configFilePath = path.resolve(process.cwd(), configFile)
+
+/**
+ * @function returnConfig
+ * @description Reads the config file and returns the user config
+ * @returns {Types.Config}
+ */
+const returnConfig = () => {
+	if (fs.existsSync(configFilePath)) {
+		const configFileContent = fs.readFileSync(configFilePath, 'utf8')
+		return JSON.parse(configFileContent)
+	} else {
+		throw new Error(`[🔴] No config file found. Please create a ${configFile} file in the root of your project.`)
+	}
+}
+
+const config = returnConfig()
+
+import { defaultConfig } from './ri18next.config.js'
 import { loadJsonFiles, loadTranslation, readFiles, getCodeKeys } from './utils.js'
 import { checkForDuplicateValues } from './scripts/duplicates.js'
 import { missingKeysInCode, missingKeysInTranslation } from './scripts/missing.js'
@@ -8,18 +31,28 @@ console.info('----------------------------------------')
 console.info('[🟡] Starting react-i18next-validator...')
 console.info('----------------------------------------')
 
-const files = readFiles(config.src)
-const codeKeys = files.map(file => getCodeKeys(file, config)).flat()
-const translationFiles = loadJsonFiles(config)
+/**
+ * @type {Types.Config}
+ */
+const mergedConfig = {
+	errorLevel: config?.errorLevel || defaultConfig.errorLevel,
+	src: config.src,
+	directory: config.directory,
+	regex: config?.regex ? [...config.regex, ...defaultConfig.regex] : defaultConfig.regex
+}
+
+const files = readFiles(mergedConfig.src)
+const codeKeys = files.map(file => getCodeKeys(file, mergedConfig)).flat()
+const translationFiles = loadJsonFiles(mergedConfig)
 
 for (const translationFile of translationFiles) {
 	const translation = loadTranslation(translationFile)
 	console.info('[🟡] Starting comparison...')
 	console.info('----------------------------------------')
 
-	checkForDuplicateValues(translation, config)
-	missingKeysInCode(translation, codeKeys, config)
-	missingKeysInTranslation(translation, codeKeys, config)
+	checkForDuplicateValues(translation, mergedConfig)
+	missingKeysInCode(translation, codeKeys, mergedConfig)
+	missingKeysInTranslation(translation, codeKeys, mergedConfig)
 }
 
 console.info('[✅] Comparison finished. All good.')
--- ri18next.config.js
+++ ri18next.config.js
@@ -1,10 +1,11 @@
 /**
- * @typedef {{directory: string, errorLevel: 'warn'|'error'|'off', src: string}} Config
- * @description The config for the validator
- * @property {string} directory The path to the translation file
+ * @typedef {{errorLevel?: 'warn'|'error'|'off', regex?: Array<string>, src?: string, directory?: string}} Config
+ * @description The default config for the validator
+ * @property {string} src The source folder
  * @property {'warn'|'error'|'off'} errorLevel The error level
- * @property {string} src The path to the source files
  * @property {Array<string>} regex The regex to find the translation keys in the source files
+ * @property {string} src The source folder
+ * @property {string} directory The directory of the translation files
  */
 
 export const Types = {}
@@ -12,9 +13,7 @@ export const Types = {}
 /**
  * @type {Config}
  */
-export const config = {
-	directory: '../test/public/locales/',
+export const defaultConfig = {
 	errorLevel: 'error',
-	src: '../test/src',
 	regex: ["t[(][`']([a-zA-z0-9.]*)[`'][,)]", "i18nKey=[`'\"]([^']+)[`'\"]"]
 }
